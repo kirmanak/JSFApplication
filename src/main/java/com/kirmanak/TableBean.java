a0797a0031ffabeacfb8345eed34174baec3fc29
Kirill Kamakin <kirmanak@gmail.com>
2017-11-01 02:04:59 +0300

    Looks like input is almost ready

--- src/main/java/com/kirmanak/TableBean.java
+++ src/main/java/com/kirmanak/TableBean.java
@@ -55,6 +55,7 @@ public class TableBean implements Serializable {
 
     @PreDestroy
     private void removeSession () {
+        if (true) throw new RuntimeException("Hello, world!");
         /*
         ORM.removeSession(getSessionId());
         */
