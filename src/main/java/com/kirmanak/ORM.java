2c10e48ca85195cb0bf01033de20d8d182ebabd7
Kirill Kamakin <kirmanak@gmail.com>
2017-10-31 21:53:53 +0300

    Form is almost working -.-

--- src/main/java/com/kirmanak/ORM.java
+++ src/main/java/com/kirmanak/ORM.java
@@ -5,11 +5,11 @@ import org.hibernate.Session;
 import java.util.List;
 
 public class ORM {
-	public void insert(final RowBean row) {
+	public static void insert(final RowBean row) {
         final String sessionId = row.getSessionId();
 		final Session session = HibernateUtil.getSessionFactory().openSession();
     	session.beginTransaction();
-     	session.save(row);
+     	// session.save(row);
      	// save session if it doesn't exist, 
      	// connect row with it's session 
      	// by Many-To-One connection
@@ -17,7 +17,7 @@ public class ORM {
      	HibernateUtil.shutdown();
 	}
 
-	public List<RowBean> getRows (final String sessionId) {
+	public static List<RowBean> getRows (final String sessionId) {
         final Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
         // load every row connected with current session
@@ -27,7 +27,7 @@ public class ORM {
         return results;
 	}
 
-	public void removeSession (final String sessionId) {
+	public static void removeSession (final String sessionId) {
 		// here you have to remove the session and
 		// each row, which connected to the session
 	}
